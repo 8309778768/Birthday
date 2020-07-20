f29d9d710e98fb4271e8b5804fd18bc98dbb8546
adi lak <adi@gmail.com>
2020-07-20 13:37:31 +0530

    firstcommit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=0=(2558j#1n5ggrrgl8xrcp846hg-4(t8of+f*l26@-9pm)=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
